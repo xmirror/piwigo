13f41e982d3e82e34d5241b037c8692c00c796c9
(no author) <(no author)@68402e56-0260-453c-a942-63ccdbb3a9ee>
2003-09-17 20:40:15 +0000

    This commit was manufactured by cvs2svn to create tag 'bug-1_3beta-011'.
    git-svn-id: http://piwigo.org/svn/tags/bug-1_3beta-011@131 68402e56-0260-453c-a942-63ccdbb3a9ee

--- template/default/theme/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-	/*---------------couleur de fond pour le template agjimmy - lightred------------------*/
-	/* ce template a été "adapté" par thierry                                         */
-	$user['couleur_fond'] = "#FFFFFF";
-	$user['couleur_text_fond'] = "black";
-	$user['couleur_text'] = "black";
-?>
